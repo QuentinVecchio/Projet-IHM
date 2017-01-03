729ed7201be0f3970826d7642451f010f1495163
Quentin Vecchio <vecchioquentin@hotmail.fr>
2017-01-03 09:38:37 +0100

    Ajout des différentes pages + API Google MAPS

--- Android/RestIn/app/src/main/java/com/insa_lyon/restin/MapActivity.java
+++ Android/RestIn/app/src/main/java/com/insa_lyon/restin/MapActivity.java
@@ -1,13 +1,60 @@
 package com.insa_lyon.restin;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 
 public class MapActivity extends AppCompatActivity {
+    MapView mapView;
+    //GoogleMap map;
+    ListView listView;
+    EditText searchEditText;
+    Button filterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        //MapView
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        //SearchBar
+        searchEditText = (EditText) findViewById(R.id.searchEditText);
+        filterButton = (Button) findViewById(R.id.filtreButton);
+
+        //ListView
+        listView = (ListView) findViewById(R.id.restaurantListView);
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
