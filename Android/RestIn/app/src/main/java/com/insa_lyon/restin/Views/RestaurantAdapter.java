8f646d9f6623134e314f7b032b712e44b05b0f42
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-01-06 08:58:37 +0100

    Correction RestaurantAdapter

--- Android/RestIn/app/src/main/java/com/insa_lyon/restin/Views/RestaurantAdapter.java
+++ Android/RestIn/app/src/main/java/com/insa_lyon/restin/Views/RestaurantAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
 import com.insa_lyon.restin.Modeles.Restaurant;
 import java.util.List;
 import com.insa_lyon.restin.R;
@@ -14,39 +15,22 @@ import com.insa_lyon.restin.R;
  */
 
 public class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+
+    private List<Restaurant> restaurants = null;
+
     public RestaurantAdapter(Context context, List<Restaurant> restaurants) {
         super(context, 0, restaurants);
+        this.restaurants = restaurants;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_tweet,parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.restaurant_layout,parent, false);
         }
 
-        TweetViewHolder viewHolder = (TweetViewHolder) convertView.getTag();
-        if(viewHolder == null){
-            viewHolder = new TweetViewHolder();
-            viewHolder.pseudo = (TextView) convertView.findViewById(R.id.pseudo);
-            viewHolder.text = (TextView) convertView.findViewById(R.id.text);
-            viewHolder.avatar = (ImageView) convertView.findViewById(R.id.avatar);
-            convertView.setTag(viewHolder);
-        }
-
-        Restaurant restaurant = getItem(position);
-
-        //il ne reste plus qu'à remplir notre vue
-        viewHolder.pseudo.setText(tweet.getPseudo());
-        viewHolder.text.setText(tweet.getText());
-        viewHolder.avatar.setImageDrawable(new ColorDrawable(tweet.getColor()));
 
         return convertView;
-    }
-
-    private class TweetViewHolder{
-        public TextView pseudo;
-        public TextView text;
-        public ImageView avatar;
     }
 }
