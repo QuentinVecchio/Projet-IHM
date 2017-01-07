44e183f8bc5ec34f51f85302b07e43e2703eeabf
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2017-01-07 17:44:30 +0100

    Ajout écran de lancement et logo

---
+++ Android/RestIn/app/src/main/java/com/insa_lyon/restin/Views/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.insa_lyon.restin.Views;
+
+/**
+ * Created by Nico on 07/01/2017.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(this, MapActivity.class);
+        this.startActivity(intent);
+        this.finish();
+    }
+}
