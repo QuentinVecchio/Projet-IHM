ee2dcab12416fc00369097700689d59a5e193d9f
LudovicCros <cros_ludovic@hotmail.fr>
2017-01-08 20:21:56 +0100

    Modification vue filter et modification version google play pour que cela fonctionne sur emulateur

--- Android/RestIn/app/src/main/java/com/insa_lyon/restin/Views/FilterActivity.java
+++ Android/RestIn/app/src/main/java/com/insa_lyon/restin/Views/FilterActivity.java
@@ -3,17 +3,27 @@ package com.insa_lyon.restin.Views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.insa_lyon.restin.R;
 
 public class FilterActivity extends AppCompatActivity {
-
+    Button clickButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.getSupportActionBar().setTitle(R.string.filters);
+        clickButton = (Button) findViewById(R.id.button2);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* action */
+
+            }
+        });
     }
 
     @Override
